b126c6539cf677efcf35a91c7b705c1982e042a8
niftrr <niftrr@fluidnft.org>
2024-02-02 15:43:06 +0100

    published package

--- src/constants.ts
+++ src/constants.ts
@@ -1,22 +1,7 @@
 import JSBI from 'jsbi'
 
 export enum SupportedChainId {
-  MAINNET = 1,
-  GOERLI = 5,
-
-  ARBITRUM_ONE = 42161,
-  ARBITRUM_GOERLI = 421613,
-
-  OPTIMISM = 10,
-  OPTIMISM_GOERLI = 420,
-
-  POLYGON = 137,
-  POLYGON_MUMBAI = 80001,
-
-  CELO = 42220,
-  CELO_ALFAJORES = 44787,
-
-  BNB = 56
+  ARBITRUM_SEPOLIA = 421614,
 }
 
 // exports for external consumption
